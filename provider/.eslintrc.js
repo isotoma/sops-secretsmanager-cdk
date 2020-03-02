612bea178a793c9c10a2a9cd3948d2c42d267ab2
Andrew Plummer <plummer574@gmail.com>
2020-03-02 12:57:55 +0000

    tidying

--- provider/.eslintrc.js
+++ provider/.eslintrc.js
@@ -3,9 +3,6 @@ module.exports = {
   // Specifies the ESLint parser
   parser: '@typescript-eslint/parser',
   extends: [
-    // Uses the recommended rules from @eslint-plugin-react
-    // Uncomment for React projects
-    // 'plugin:react/recommended',
     // Uses the recommended rules from @typescript-eslint/eslint-plugin
     'plugin:@typescript-eslint/recommended',
     // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
@@ -25,11 +22,5 @@ module.exports = {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
-  settings: {
-    // Uncomment for React projects
-    react: {
-      // Tells eslint-plugin-react to automatically detect the version of React to use
-      version: 'detect',
-    },
-  },
+  settings: {},
 };
