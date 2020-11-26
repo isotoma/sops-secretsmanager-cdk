16aa44359d559b561589878a380fe38119a11d05
Andrew Plummer <andrew.plummer@isotoma.com>
2020-11-26 11:07:48 +0000

    Jest coverage checking (#34)

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,13 @@ module.exports = {
     transform: {
         '^.+\\.ts$': 'ts-jest',
     },
+    collectCoverage: true,
+    coverageThreshold: {
+        global: {
+            statements: 81,
+            branches: 67,
+            functions: 100,
+            lines: 81,
+        },
+    },
 };
--- provider/jest.config.js
+++ provider/jest.config.js
@@ -4,4 +4,13 @@ module.exports = {
     transform: {
         '^.+\\.ts$': 'ts-jest',
     },
+    collectCoverage: true,
+    coverageThreshold: {
+        global: {
+            statements: 81,
+            branches: 64,
+            functions: 88,
+            lines: 81,
+        },
+    },
 };
