425ab1df1963e74423f8895f2e66d25b2222e96f
Andrew Plummer <andrew.plummer@isotoma.com>
2023-05-31 11:58:37 +0100

    Use CDKv2 for example (#105)

--- example/sops-example/bin/sops-example.ts
+++ example/sops-example/bin/sops-example.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from 'aws-cdk-lib';
 import { SopsExampleStack } from '../lib/sops-example-stack';
 
 const app = new cdk.App();
--- example/sops-example/lib/sops-example-stack.ts
+++ example/sops-example/lib/sops-example-stack.ts
@@ -1,12 +1,11 @@
-import * as cdk from '@aws-cdk/core';
-import * as secretsmanager from '@aws-cdk/aws-secretsmanager';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as customResource from '@aws-cdk/custom-resources';
+import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
-import { SopsSecretsManager } from './sops-secretsmanager-cdk-dev/cdkv1';
+import { SopsSecretsManager } from './sops-secretsmanager-cdk-dev';
 
 export class SopsExampleStack extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         const secret = new secretsmanager.Secret(this, 'TestSecret');
@@ -23,16 +22,6 @@ export class SopsExampleStack extends cdk.Stack {
                 },
             },
         });
-
-        // This section hacks the CDK's utility lambda to use Node 12,
-        // which uses Node 10 in cdk <1.94.0. This is no longer
-        // deployable as of July 30, 2021.
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const providerWrapper = cdk.Stack.of(this).node.findChild('com.isotoma.cdk.custom-resources.sops-secrets-manager') as any;
-        const provider = providerWrapper.provider as customResource.Provider;
-        const lambdaFn = (provider.node.findChild('framework-onEvent') as unknown) as lambda.Function;
-        const cfnLambdaFn = lambdaFn.node.defaultChild as lambda.CfnFunction;
-        cfnLambdaFn.addPropertyOverride('Runtime', lambda.Runtime.NODEJS_12_X.toString());
 
         new cdk.CfnOutput(this, 'TestSecretArn', {
             value: secret.secretArn,
